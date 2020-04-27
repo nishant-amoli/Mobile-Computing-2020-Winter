eb0555564e2b1d246d37a62f88d65d974f6da245
Nishant Amoli <nishant.amoli@dal.ca>
2020-04-27 12:57:45 -0300

    Group Project committed

---
+++ Group Project- SmartShopper/SmartShopper Android Application/SmartShopper/README.md
@@ -0,0 +1,56 @@
+# SmartShopper
+
+SmartShopper is an android mobile application, that helps users to track the best prices of products from various stores. It also has a shopping list that removes the hassle of remembering what to buy. the barcode scanning feature is implemented for user convenience and faster search.
+
+## Installation
+
+Android Studio 3.X or above.
+
+Android SDK version 23 or above.
+
+## Usage
+Clone master branch and run with Android Studio in emulator or android device.
+## Library
+[Glide](https://github.com/bumptech/glide) - For showing GIFs in Imageview.
+
+[Barcode](https://github.com/journeyapps/zxing-android-embedded) - For barcode scanning.
+ 
+[Folding Cell Desgin](https://github.com/Ramotion/folding-cell-android) - Recycler view with folding cell design.
+
+[Intro Slides](https://github.com/heinrichreimer/material-introa) - To make introduction slides ane helper slides.
+
+
+[Gson](https://mvnrepository.com/artifact/com.google.code.gson/gson/2.8.6
+) - For storing data in SharedPreference.
+
+[Firebase](https://firebase.google.com/docs/libraries) - For storing and retrieving products/stores information and images.  
+## Product Description
+[Milk](https://www.walmart.ca/en/ip/scotsburn-2-milk-jug/6000196944428)
+
+[Vaseline](https://www.vaseline.com/ca/en/products/lotions-and-moisturizers/vaseline-lotion-aloe.html)
+
+[Ivory Body Wash](
+https://www.dollargeneral.com/ivory-body-wash-original-scent-30-oz.html)
+
+## Company Logos used
+[Walmart](https://www.walmart.com/)
+
+[Sobeys](https://www.sobeys.com/en/)
+
+[nofrills](https://www.nofrills.ca/)
+
+[Bigs Grocery](https://bigstorehalifax.com/)
+
+[Costco](https://www.costco.ca/)
+
+[Dmart](https://www.dmartindia.com/)
+
+[Atlantic superstore](https://www.atlanticsuperstore.ca/)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update the tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Group Project- SmartShopper/SmartShopper Android Application/SmartShopper/app/src/main/java/com/example/smartshopper/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.smartshopper
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
+
+lateinit var GLOBAL_BARCODE: String
+
+class MainActivity : AppCompatActivity() {
+    val INTRO_COMPLETED = "IntroCompleted"
+    val FIRST_PREF_NAME = "FirstLaunchPref"
+    val REQUEST_CODE_INTRO = 1
+
+
+    lateinit var sharedPref: SharedPreferences
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_shoppinglist, R.id.navigation_home, R.id.navigation_barcode
+            )
+        )
+
+        //supportActionBar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        // chechk for first install and intro completed or not and save it in shared preference
+        sharedPref = getSharedPreferences(FIRST_PREF_NAME, Context.MODE_PRIVATE)
+        if (!sharedPref.getBoolean(INTRO_COMPLETED, false)) {
+            startActivityForResult(
+                Intent(baseContext, IntroActivity::class.java),
+                REQUEST_CODE_INTRO
+            )
+        }
+        GLOBAL_BARCODE = ""
+        var codetoscan = intent.getStringExtra("barcode")
+        if (!codetoscan.isNullOrEmpty()) {
+            GLOBAL_BARCODE = codetoscan
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_INTRO) {
+            sharedPref = getSharedPreferences(FIRST_PREF_NAME, Context.MODE_PRIVATE)
+            if (resultCode == Activity.RESULT_OK) {
+                with(sharedPref.edit()) {
+                    putBoolean(INTRO_COMPLETED, true)
+                    commit()
+                }
+            } else {
+                with(sharedPref.edit()) {
+                    putBoolean(INTRO_COMPLETED, false)
+                    commit()
+                }
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.navbar_help) {
+            startActivity(Intent(baseContext, HelpActivity::class.java))
+        }
+        return true
+    }
+
+}
